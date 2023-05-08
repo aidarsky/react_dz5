a549fe6ac13af700e5f3265824b5106a3aa3c464
aidarsky <122036072+aidarsky@users.noreply.github.com>
2023-05-08 20:58:29 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      time: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Текущее время:</h1>
+        <h2>{this.state.time.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
 }
 
-export default App;
+class UrlWatcher extends React.Component {
+  componentDidMount() {
+    this.unlisten = this.props.history.listen((location, action) => {
+      console.log(`URL изменился на ${location.pathname}${location.search}`);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unlisten();
+  }
+
+  render() {
+    return null;
+  }
+}
+
+
+export default Timer
+
